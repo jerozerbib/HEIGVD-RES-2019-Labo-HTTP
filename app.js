761679420dc62723775105f755ca9f1ed126d5d7
Olivier Koffi <o.koffi021194@gmail.com>
2019-05-19 15:41:04 +0200

    Simple HTTP Client done

---
+++ app.js
@@ -0,0 +1,9 @@
+// Includes
+var request = require('request');
+
+// Core
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
